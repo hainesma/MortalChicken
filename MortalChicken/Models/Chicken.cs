ffd5254c0d637cc356d171b603ad5a1a644fd236
Mark Haines <hainesma@gmail.com>
2021-07-26 13:51:38 -0400

    Completed Chicken model

--- MortalChicken/Models/Chicken.cs
+++ MortalChicken/Models/Chicken.cs
@@ -12,6 +12,14 @@ namespace MortalChicken.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Smarts { get; set; }
+        public double Strength { get; set; }
+        public double Speed { get; set; }
+        public double Luck { get; set; }
+        public string Color { get; set; }
+        public int FarmId { get; set; }
 
     }
 }
