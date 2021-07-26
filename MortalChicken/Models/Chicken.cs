8aec9573e59e8c7c28f5f639cfc4a1297565b6a9
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-07-26 15:45:39 -0400

    Started farm controller

---
+++ MortalChicken/Models/Chicken.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MortalChicken.Models
+{
+    public class Chicken
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public double Smarts { get; set; }
+        public double Speed { get; set; }
+        public double Luck { get; set; }
+        public string Color { get; set; }
+        [ForeignKey("Farm")]
+        public int FarmId { get; set; }
+    }
+}
